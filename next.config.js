aeee6dd5bad38dfbd480658588faa4e9ab622989
kmlzhn <kamila.zhandildayeva2008@gmail.com>
2025-10-18 23:52:08 -0700

    .

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client']
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client')
-    }
-    return config
   }
 }
 
